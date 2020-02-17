f407ea43f9e85b0a1fcd39dde26b96cdf6641953
Mirabela Dan <mirabeladan@live.de>
2020-02-17 18:57:13 +0000

    endpoint works

--- handler.js
+++ handler.js
@@ -2,17 +2,16 @@ const express = require ("express");
 const cors = require ("cors");
 const serverlessHttp = require ("serverless-http");
 const bodyParser = require("body-parser");
-
+const destinations = require("./destinations.json");
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.get("/destinations", function(request, response) {
-  response.status(200).json({
-    tasks: ./destinations.json
-})
-}
-)
+app.get("/destinations/:city", function(req, res) {
+  const city = req.params.city;
+  var data_filter = destinations.filter(element => element.city === city);
+  res.json(data_filter);
+});
 
 module.exports.app = serverlessHttp(app);
